03558fa8729bbb4ab16153209f10a3c6b083b782
Cav Lemasters <cav.lemasters@geneva.edu>
2019-01-14 20:41:22 -0500

    My First Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel -- Welcome Cav
                 </div>
 
                 <div class="links">
